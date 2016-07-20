7411e9317ccf16db8d03dc765c1634b96279edd9
sofia zaim <sofiazaim@sofias-MacBook-Pro.local>
2016-07-20 16:37:31 -0400

    sofi's commit

--- js/app.js
+++ js/app.js
@@ -12,6 +12,42 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  /**	
+  	function newGame(){
+  		var random = Math.floor(Math.random() * 100);
+  		console.log(random);
+  	}
+
+	/*--- Generate Random Number ---*/
+/**	function generateNumber() {
+
+		var generatedNumber = Math.floor((Math.random()*100)+1);
+		console.log("Generated Random Number = "+ generatedNumber);
+
+		return generatedNumber;
+	}
+
+  	newGame();
+  	**/
+  	
+  	var count = 1
+  	$("form").submit(function(e){
+  		e.preventDefault();
+  		var guessNum = $("#userGuess").val();
+  		if (guessNum >= 0 && guessNum <= 100){
+	  		$("#guessList").append("<li>" + guessNum + "</li>");
+	  		$("#count").text(count++);
+  		} else (alert("this Number is not valide!"));
+  		// Generate the random number
+  		randomNum();
+  		
+  	});
+  	// this function will generate the random number that we invoke in the submit 
+  	var randomNum = function(){
+		var random = Math.floor((Math.random() * 100) + 1);
+		console.log("the Random Number is = " + random);
+		return random;
+  	}
+
+
 });
-
-
